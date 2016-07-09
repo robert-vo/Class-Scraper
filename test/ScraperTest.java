f5d57f6ab840209f6b5870a714a66349484e3dd8
robert-vo <robertvo79@gmail.com>
2016-07-09 12:45:28 -0500

    Added working canary test

---
+++ test/ScraperTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Robert on 7/9/16.
+ */
+public class ScraperTest {
+
+    @Test
+    public void canaryTest() {
+        assertTrue(true);
+    }
+}
