3458c0ce2d2a103fdfebc7d01e6a538d789342f1
Robert Vo <robertvo79@gmail.com>
2016-07-11 10:01:37 -0500

    Create WebScraper.java

---
+++ src/scraper/WebScraper.java
@@ -0,0 +1,19 @@
+//package main.java.com.robertvo.scraper;
+
+import java.util.Scanner;
+
+/**
+ * The purpose of this class is to use the internet to scrape the classbrowser pages.
+ * This class will use the Scraper class to assist in completing this task.
+ */
+public class WebScraper {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter ");
+
+//        final String websiteToScrape = URLBuilder.createURLForTermOnly("1990");
+    }
+
+}
