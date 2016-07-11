807a76005a3e41af9119838ab4df7c494607f2b1
robert-vo <robertvo79@gmail.com>
2016-07-10 20:12:26 -0500

    Initial commit - new enumerated type.

---
+++ src/URLParameters.java
@@ -0,0 +1,11 @@
+
+/*
+
+These are the parameters for the class browser.
+The parameters are case-sensitive, that is why they will remain lower-cased.
+
+*/
+
+public enum URLParameters {
+    term, subject, location, mode, distance
+}
