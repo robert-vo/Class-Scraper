71168dc48c6061b433e7fb54f9878944290b2007
robert-vo <robertvo79@gmail.com>
2016-07-11 20:17:36 -0500

    Filled in more constants for the elements.

--- src/scraper/HTMLElements.java
+++ src/scraper/HTMLElements.java
@@ -5,7 +5,26 @@ package scraper;
  */
 public enum HTMLElements {
     RETRIEVE_ALL_CLASSES("ul[id=accordion] > li"),
-    NUMBER_OF_CLASSES   ("div > section > article > section > h1");
+    NUMBER_OF_CLASSES   ("div > section > article > section > h1"),
+    NAME_AND_CRN_NUMBER ("h3[class=class-title] > strong"),
+    COURSE_TITLE        ("span[class=class-title--course]"),
+    CLASS_STATUS        ("span[class^=class--status]"),
+    CLASS_ATTRIBUTES    ("small[class=class--attributesList] > span"),
+    CLASS_START_DATE    (""),
+    CLASS_END_DATE      (""),
+    CLASS_DAYS          (""),
+    CLASS_TIMES         (""),
+    INSTRUCTOR          (""),
+    INSTRUCTOR_EMAIL    (""),
+    LOCATION            (""),
+    ROOM                (""),
+    FORMAT              (""),
+    DESCRIPTION         (""),
+    CLASS_DURATION      (""),
+    SESSION             (""),
+    CLASS_COMPONENT     (""),
+    CLASS_SYLLABUS      (""),
+    NOTES               ("");
 
     final private String html;
 
