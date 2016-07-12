d6dbd2a196f99b84f35806bfa281b609efe17825
robert-vo <robertvo79@gmail.com>
2016-07-11 23:00:24 -0500

    Added html for more enums.

--- src/scraper/HTMLElements.java
+++ src/scraper/HTMLElements.java
@@ -4,27 +4,25 @@ package scraper;
  * Created by Robert on 7/11/16.
  */
 public enum HTMLElements {
-    RETRIEVE_ALL_CLASSES("ul[id=accordion] > li"),
-    NUMBER_OF_CLASSES   ("div > section > article > section > h1"),
-    NAME_AND_CRN_NUMBER ("h3[class=class-title] > strong"),
-    COURSE_TITLE        ("span[class=class-title--course]"),
-    CLASS_STATUS        ("span[class^=class--status]"),
-    CLASS_ATTRIBUTES    ("small[class=class--attributesList] > span"),
-    CLASS_START_DATE    (""),
-    CLASS_END_DATE      (""),
-    CLASS_DAYS          (""),
-    CLASS_TIMES         (""),
-    INSTRUCTOR          (""),
-    INSTRUCTOR_EMAIL    (""),
-    LOCATION            (""),
-    ROOM                (""),
-    FORMAT              (""),
-    DESCRIPTION         (""),
-    CLASS_DURATION      (""),
-    SESSION             (""),
-    CLASS_COMPONENT     (""),
-    CLASS_SYLLABUS      (""),
-    NOTES               ("");
+    RETRIEVE_ALL_CLASSES        ("ul[id=accordion] > li"),
+    NUMBER_OF_CLASSES           ("div > section > article > section > h1"),
+    CLASS_NAME_AND_CRN_NUMBER   ("h3[class=class-title] > strong"),
+    COURSE_TITLE                ("span[class=class-title--course]"),
+    CLASS_STATUS                ("span[class^=class--status]"),
+    CLASS_ATTRIBUTES            ("small[class=class--attributesList] > span"),
+    CLASS_DATES                 ("li[class=float-list--row] :eq(0) > span[class$=body]"),
+    CLASS_DAYS_TIMES            ("li[class=float-list--row] :eq(1) > span[class$=body]"),
+    CLASS_INSTRUCTOR            ("li[class=float-list--row] :eq(0) > span[class$=body] > a"),
+    CLASS_INSTRUCTOR_EMAIL      ("li[class=float-list--row] :eq(0) > span[class$=body] > a"),
+    CLASS_LOCATION              ("li[class=float-list--row] > ul > li > span[class=class-info-body]"),
+    CLASS_ROOM                  ("li[class=float-list--row] > ul > li > span[class=class-info-body]"),
+    CLASS_FORMAT                (""),
+    CLASS_DESCRIPTION           (""),
+    CLASS_DURATION              (""),
+    CLASS_SESSION               (""),
+    CLASS_COMPONENT             (""),
+    CLASS_SYLLABUS              (""),
+    CLASS_NOTES                 ("");
 
     final private String html;
 
