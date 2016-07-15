a529df9212bafb71b559561a32e1a971aae8deda
robert-vo <robertvo79@gmail.com>
2016-07-15 18:04:41 -0500

    Refactored the function, extractTextBetweenParentheses, to take into account exception handling.

--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -4,9 +4,26 @@ import org.jsoup.select.Elements;
 
 public interface Scraper {
     String REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES = "[\\(\\)]";
+    String REGEX_TO_GET_EMAIL_FROM_HREF_TAG       = "[\\t\\n]";
 
     static String extractTextBetweenParentheses(Elements e) {
-        return e.first().text().split(REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES)[1];
+        try {
+            return e.first().text().split(REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES)[1];
+        }
+        catch (Exception e1) {
+            return "0";
+        }
+    }
+
+    static String extractEmailFromHREFTag(Elements e) {
+        try {
+            return e.attr("href")
+                    .replaceAll(REGEX_TO_GET_EMAIL_FROM_HREF_TAG, "")
+                    .substring(7);
+        }
+        catch (Exception e1) {
+            return "Unknown Email";
+        }
     }
 
 }
