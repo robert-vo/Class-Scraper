1263afbde47a85c8758d8416aacfd48b0c996cac
robert-vo <robertvo79@gmail.com>
2016-07-15 20:37:21 -0500

    Added page parameter

--- src/scraper/URLParameters.java
+++ src/scraper/URLParameters.java
@@ -7,5 +7,5 @@ The parameters are case-sensitive, that is why they will remain lower-cased.
 */
 
 public enum URLParameters {
-    term, subject, location, mode, distance
+    term, subject, location, mode, distance, page
 }
