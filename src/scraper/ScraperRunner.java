4d34f6aa5e2ef539668d50cfa5fc8f79c8e4d656
robert-vo <robertvo79@gmail.com>
2016-07-15 20:37:45 -0500

    Merged WebScraper into ScraperRunner

--- src/scraper/ScraperRunner.java
+++ src/scraper/ScraperRunner.java
@@ -1,5 +1,6 @@
 package scraper;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -13,23 +14,19 @@ import static scraper.ScrapeElements.*;
 public class ScraperRunner implements Scraper {
 
     private int numberOfClasses;
-    final private Document WEBSITE_TO_SCRAPE;
+    private static String WEBSITE_URL;
+    private Document WEBSITE_TO_SCRAPE;
 
-    public ScraperRunner(Term term) throws IOException {
-        WEBSITE_TO_SCRAPE = generateDocumentForTerm(term);
+    public ScraperRunner(Term term) {
+        WEBSITE_URL = URLBuilder.createURLForTermOnly(term);
     }
 
     public Document getWebsiteToScrape() {
         return WEBSITE_TO_SCRAPE;
     }
 
-    public void startScraper(Term term) throws IOException {
-        numberOfClasses = getNumberOfClasses(WEBSITE_TO_SCRAPE);
-        ScraperGUI.appendToLoggerTextArea("Processing " + numberOfClasses + " classes.");
-    }
-
     public static int getNumberOfClasses(Document document) {
-        Elements numberOfClassesInHTML = document.select(HTMLElements.CLASS_NAME_AND_CRN_NUMBER.getHtml());
+        Elements numberOfClassesInHTML = document.select(HTMLElements.NUMBER_OF_CLASSES.getHtml());
         return Integer.parseInt(Scraper.extractTextBetweenParentheses(numberOfClassesInHTML));
     }
 
@@ -43,26 +40,28 @@ public class ScraperRunner implements Scraper {
         return allClasses.size() != 0;
     }
 
-    public Class getAllClassInformationFromDocument(Document document) {
-
-        return new Class();
-    }
-
-    public static void getASingleClassInformation() throws IOException {
-        File input = new File("test/resources/coscPageOne.html");
-        Document doc = Jsoup.parse(input, "UTF-8", "");
-        Elements allClasses = doc.select("ul[id=accordion] > li");
-
-        System.out.println(getNameAndCourseNumber(allClasses.get(0)));
-        System.out.println(getCourseTitle(allClasses.get(0)));
-        System.out.println(getCourseStatus(allClasses.get(0)));
-        System.out.println(getClassAttributes(allClasses.get(0)));
+    public static Document retrieveWebpage(String URL) throws IOException {
+        Connection.Response response = Jsoup.connect(URL)
+                .ignoreContentType(true)
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                .referrer("http://www.google.com")
+                .timeout(12000)
+                .followRedirects(true)
+                .execute();
+        return response.parse();
     }
 
     public static void main(String[] args) throws IOException {
-        getASingleClassInformation();
+
+
+//        System.out.println(ScraperRunner.getNumberOfClasses(entireWebpage));
+//
+//        ScraperGUI.appendToLoggerTextArea("Processing " + ScraperRunner.getNumberOfClasses(entireWebpage) + " classes.");
+
     }
 
-
+    public static void run() throws IOException {
+        Document entireWebPage = retrieveWebpage(WEBSITE_URL);
+    }
 
 }
