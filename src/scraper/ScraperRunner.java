4ac96e62a49dfe03e7426a324bb2e7545c1b0300
robert-vo <robertvo79@gmail.com>
2016-07-16 21:36:29 -0500

    Made verifyValidDocument check for NullPointerExceptions. Added run method that will start the scraping. Added a database method that will connect to the, currently local, database and insert data into the database.

--- src/scraper/ScraperRunner.java
+++ src/scraper/ScraperRunner.java
@@ -6,22 +6,26 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ui.ScraperGUI;
-import java.io.File;
+import java.sql.*;
 import java.io.IOException;
-
-import static scraper.ScrapeElements.*;
 
 public class ScraperRunner implements Scraper {
 
-    private int numberOfClasses;
     private static String WEBSITE_URL;
     private Document WEBSITE_TO_SCRAPE;
+    private static Term term;
 
     public ScraperRunner(Term term) {
         WEBSITE_URL = URLBuilder.createURLForTermOnly(term);
+        this.term = term;
     }
 
-    public Document getWebsiteToScrape() {
+    public static Term getTerm() {
+        return term;
+    }
+
+    public Document getWebsiteToScrape() throws IOException {
+        WEBSITE_TO_SCRAPE = retrieveWebpage(WEBSITE_URL);
         return WEBSITE_TO_SCRAPE;
     }
 
@@ -36,8 +40,13 @@ public class ScraperRunner implements Scraper {
     }
 
     public static boolean verifyValidDocument(Document document) {
-        Elements allClasses = document.select(HTMLElements.RETRIEVE_ALL_CLASSES.getHtml());
-        return allClasses.size() != 0;
+        try {
+            Elements allClasses = document.select(HTMLElements.RETRIEVE_ALL_CLASSES.getHtml());
+            return allClasses.size() != 0;
+        }
+        catch (NullPointerException e) {
+            return false;
+        }
     }
 
     public static Document retrieveWebpage(String URL) throws IOException {
@@ -51,17 +60,97 @@ public class ScraperRunner implements Scraper {
         return response.parse();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void run() throws IOException {
+        try {
+
+            Document entireWebPage = retrieveWebpage(WEBSITE_URL);
+
+            int poop = 0;
+            while (verifyValidDocument(entireWebPage) && (poop < 5)) {
+                Elements allClasses = entireWebPage.select(HTMLElements.RETRIEVE_ALL_CLASSES.getHtml());
+                for (Element aClass : allClasses) {
+                    Term termID = getTerm();
+                    String classTitle = ScrapeElements.getCourseTitle(aClass);
+                    String className = ScrapeElements.getCourseName(aClass);
+                    Class.Status classStatus = ScrapeElements.getCourseStatusOpenOrClosed(aClass);
+                    String courseNumber = ScrapeElements.getCourseNumber(aClass);
+                    int seatsTaken = ScrapeElements.getNumberOfSeatsTaken(aClass);
+                    int seatsAvailable = ScrapeElements.getNumberOfAvailableSeats(aClass);
+                    Semester semester = getTerm().getSemester();
+                    String classDates = ScrapeElements.getClassDates(aClass);
+                    String attributes = ScrapeElements.getClassAttributes(aClass);
+                    String daysTimes = ScrapeElements.getClassDaysAndTimes(aClass);
+                    String instructorName = ScrapeElements.getInstructorName(aClass);
+                    String instructorEmail = ScrapeElements.getInstructorEmail(aClass);
+                    System.out.println(instructorEmail);
+                    String location = ScrapeElements.getLocation(aClass);
+                    String room = ScrapeElements.getRoom(aClass);
+                    String format = ScrapeElements.getFormat(aClass);
+                    String description = ScrapeElements.getDescription(aClass);
+                    String duration = ScrapeElements.getClassDuration(aClass);
+                    String session = ScrapeElements.getSession(aClass);
+                    String component = ScrapeElements.getClassComponent(aClass);
+                    String syllabus = ScrapeElements.getSyllabus(aClass);
+                    Class c = new Class(termID, classTitle, className, classStatus, courseNumber, seatsTaken,
+                            seatsAvailable, semester, classDates, attributes, daysTimes, instructorName,
+                            instructorEmail, location, room, format, description, duration, session,
+                            component, syllabus);
+                    insertIntoDatabase(c);
+
+                }
+                poop++;
+                WEBSITE_URL = URLBuilder.incrementPageNumber(WEBSITE_URL);
+                entireWebPage = retrieveWebpage(WEBSITE_URL);
+                ScraperGUI.appendToLoggerTextArea("Starting next page: " + WEBSITE_URL);
+            }
 
 
-//        System.out.println(ScraperRunner.getNumberOfClasses(entireWebpage));
-//
-//        ScraperGUI.appendToLoggerTextArea("Processing " + ScraperRunner.getNumberOfClasses(entireWebpage) + " classes.");
-
+            ScraperGUI.appendToLoggerTextArea("Finished Scraping");
+        }
+        catch (Exception e1) {
+            ScraperGUI.appendToLoggerTextArea(e1.getMessage());
+        }
     }
 
-    public static void run() throws IOException {
-        Document entireWebPage = retrieveWebpage(WEBSITE_URL);
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost/class?useSSL=false";
+    static final String USER = "root";
+    static final String PASS = "password";
+
+    public static void insertIntoDatabase(Class c) throws SQLException, ClassNotFoundException {
+        java.lang.Class.forName(JDBC_DRIVER);
+
+        try (java.sql.Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+            String sql = "INSERT INTO CLASS(Term_ID, Semester, Title, CRN, Name, Status, Attributes, Dates, " +
+                    "Days_Times, Instructor, Instructor_Email, Location, Room, Format, Description, Duration, " +
+                    "Session, Component, Syllabus) " +
+                    "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, c.getTermID().getTermID());
+            preparedStatement.setString(2, c.getSemester().toString());
+            preparedStatement.setString(3, c.getClassTitle());
+            preparedStatement.setString(4, c.getCourseNumber());
+            preparedStatement.setString(5, c.getClassName());
+            preparedStatement.setString(6, c.getClassStatus().toString());
+            preparedStatement.setString(7, c.getAttributes());
+            preparedStatement.setString(8, c.getClassDates());
+            preparedStatement.setString(9, c.getDaysTimes());
+            preparedStatement.setString(10, c.getInstructorName());
+            System.out.println(c.getInstructorEmail());
+            preparedStatement.setString(11, c.getInstructorEmail());
+            preparedStatement.setString(12, c.getLocation());
+            preparedStatement.setString(13, c.getRoom());
+            preparedStatement.setString(14, c.getFormat());
+            preparedStatement.setString(15, c.getDescription());
+            preparedStatement.setString(16, c.getDuration());
+            preparedStatement.setString(17, c.getSession());
+            preparedStatement.setString(18, c.getComponent());
+            preparedStatement.setString(19, c.getSyllabus());
+            preparedStatement.executeUpdate();
+            ScraperGUI.appendToLoggerTextArea(preparedStatement.toString());
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
     }
 
 }
