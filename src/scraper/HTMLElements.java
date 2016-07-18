b1720cb87691160acfdabfa4777aef9d8f6d3337
robert-vo <robertvo79@gmail.com>
2016-07-17 21:55:40 -0500

    Removed comments

--- src/scraper/HTMLElements.java
+++ src/scraper/HTMLElements.java
@@ -1,8 +1,5 @@
 package scraper;
 
-/**
- * Created by Robert on 7/11/16.
- */
 public enum HTMLElements {
     RETRIEVE_ALL_CLASSES        ("ul[id=accordion] > li"),
     NUMBER_OF_CLASSES           ("div > section > article > section > h1"),
