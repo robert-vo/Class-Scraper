a65e2702e8c27c5163b356fd9095191e7dc6cea1
robert-vo <robertvo79@gmail.com>
2016-07-20 14:47:18 -0500

    Moved ScraperTest to ScraperRunnerTest.

---
+++ test/scraper/ScraperRunnerTest.java
@@ -0,0 +1,64 @@
+package scraper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static scraper.ScraperRunner.getNumberOfClasses;
+import static scraper.ScraperRunner.verifyValidDocument;
+
+public class ScraperRunnerTest {
+
+    private Document returnDocumentFromFileName(String fileName) throws IOException {
+        File page = new File("test/resources/" + fileName + ".html");
+        return Jsoup.parse(page, "UTF-8", "");
+    }
+
+    @Test
+    public void canaryTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testValidDocumentReturnsFalseForEmptyPage() throws IOException {
+        Document document = returnDocumentFromFileName("empty");
+        assertFalse(verifyValidDocument(document));
+    }
+
+    @Test
+    public void testValidDocumentReturnsTrueForValidPage() throws IOException {
+        Document document = returnDocumentFromFileName("coscPageOne");
+        assertTrue(verifyValidDocument(document));
+    }
+
+    @Test
+    public void testValidDocumentReturnsFalseForInvalidPage() throws IOException {
+        Document document = returnDocumentFromFileName("validPageWithNoClasses");
+        assertFalse(verifyValidDocument(document));
+    }
+
+    @Test
+    public void testReturnNumberOfClasses68ForValidPage() throws IOException {
+        Document document = returnDocumentFromFileName("coscPageOne");
+        assertEquals(getNumberOfClasses(document), 68);
+    }
+
+    @Test
+    public void testReturnNumberOfClasses37ForValidPage() throws IOException {
+        Document document = returnDocumentFromFileName("validPageWithNoClasses");
+        assertEquals(getNumberOfClasses(document), 37);
+    }
+
+    @Test
+    public void testReturnNumberOfClasses0ForValidPage() throws IOException {
+        Document document = returnDocumentFromFileName("empty");
+        assertEquals(getNumberOfClasses(document), 0);
+    }
+
+}
