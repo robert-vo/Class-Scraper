192ce8a77c5617ea8b01c6a6c71ca6247739d518
robert-vo <robertvo79@gmail.com>
2016-07-20 15:24:11 -0500

    Added tests for null time, invalid time, and tests for valid times.

--- test/scraper/DateTimeUtilitiesTest.java
+++ test/scraper/DateTimeUtilitiesTest.java
@@ -28,5 +28,36 @@ public class DateTimeUtilitiesTest {
         assertEquals(transformStringDateIntoSQLFormat("Aug 22, 2016").toString(), "2016-08-22");
     }
 
+    @Test
+    public void testTransformTimeIntoSQLFormatForNullTime() {
+        assertNull(transformStringTimeIntoSQLFormat(null));
+    }
+
+    @Test
+    public void testTransformTimeIntoSQLFormatForInvalidTime() {
+        assertNull(transformStringTimeIntoSQLFormat("12 12 12 12"));
+    }
+
+    @Test
+    public void testTransformTimeIntoSQLFormatForValidMidnightTime() {
+        assertEquals(transformStringTimeIntoSQLFormat("00:00 A.M.").toString(), "00:00:00");
+    }
+
+
+    @Test
+    public void testTransformTimeIntoSQLFormatForValidAMTime() {
+        assertEquals(transformStringTimeIntoSQLFormat("09:00 A.M.").toString(), "09:00:00");
+    }
+
+    @Test
+    public void testTransformTimeIntoSQLFormatForValidNoonTime() {
+        assertEquals(transformStringTimeIntoSQLFormat("12:00 P.M.").toString(), "12:00:00");
+    }
+
+    @Test
+    public void testTransformTimeIntoSQLFormatForValidPMTime() {
+        assertEquals(transformStringTimeIntoSQLFormat("5:00 P.M.").toString(), "17:00:00");
+    }
+
 
 }
