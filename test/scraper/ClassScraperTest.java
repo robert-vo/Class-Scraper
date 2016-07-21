640a3d5ddb9260968c639113383905346831779a
robert-vo <robertvo79@gmail.com>
2016-07-20 20:00:11 -0500

    Renamed file from ScraperRunnerTest.java. Added tests for invalid terms.

--- test/scraper/ClassScraperTest.java
+++ test/scraper/ClassScraperTest.java
@@ -2,6 +2,7 @@ package scraper;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,14 +11,20 @@ import java.io.IOException;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static scraper.ScraperRunner.getNumberOfClasses;
-import static scraper.ScraperRunner.verifyValidDocument;
+import static scraper.ScraperRunner.isValidDocument;
 
-public class ScraperRunnerTest {
+public class ClassScraperTest {
 
     private Document returnDocumentFromFileName(String fileName) throws IOException {
         File page = new File("test/resources/" + fileName + ".html");
         return Jsoup.parse(page, "UTF-8", "");
+    }
+
+    ClassScraper classScraper;
+
+    @Before
+    public void setUp() {
+        classScraper = new ClassScraper(Term.FALL_2016);
     }
 
     @Test
@@ -28,37 +35,40 @@ public class ScraperRunnerTest {
     @Test
     public void testValidDocumentReturnsFalseForEmptyPage() throws IOException {
         Document document = returnDocumentFromFileName("empty");
-        assertFalse(verifyValidDocument(document));
+        assertFalse(isValidDocument(document));
     }
 
     @Test
     public void testValidDocumentReturnsTrueForValidPage() throws IOException {
         Document document = returnDocumentFromFileName("coscPageOne");
-        assertTrue(verifyValidDocument(document));
+        assertTrue(isValidDocument(document));
     }
 
     @Test
     public void testValidDocumentReturnsFalseForInvalidPage() throws IOException {
         Document document = returnDocumentFromFileName("validPageWithNoClasses");
-        assertFalse(verifyValidDocument(document));
+        assertFalse(isValidDocument(document));
     }
 
     @Test
-    public void testReturnNumberOfClasses68ForValidPage() throws IOException {
-        Document document = returnDocumentFromFileName("coscPageOne");
-        assertEquals(getNumberOfClasses(document), 68);
+    public void testSetWebSiteFromTermWorks() {
+        classScraper.setWebSiteFromTerm();
+        classScraper.advanceToNextPage();
+        assertEquals(classScraper.websiteURL, "http://classbrowser.uh.edu/classes?term=2000&page=2");
     }
 
-    @Test
-    public void testReturnNumberOfClasses37ForValidPage() throws IOException {
-        Document document = returnDocumentFromFileName("validPageWithNoClasses");
-        assertEquals(getNumberOfClasses(document), 37);
+    @Test(expected = NullPointerException.class)
+    public void testAdvanceToNextPageForInvalidTerm() {
+        classScraper.setTerm(null);
+        classScraper.advanceToNextPage();
     }
 
-    @Test
-    public void testReturnNumberOfClasses0ForValidPage() throws IOException {
-        Document document = returnDocumentFromFileName("empty");
-        assertEquals(getNumberOfClasses(document), 0);
+    @Test(expected = NullPointerException.class)
+    public void testSetWebSiteForInvalidTerm() {
+        classScraper.setTerm(null);
+        classScraper.setWebSiteFromTerm();
     }
+
+
 
 }
