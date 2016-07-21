abc8ae3eedb36f2617a4ef5486af46f986eb9f9b
robert-vo <robertvo79@gmail.com>
2016-07-20 20:05:31 -0500

    Added methods that this interface needs to have implemented. Also added two strings needed for the scraper.

--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -2,4 +2,17 @@ package scraper;
 
 public interface Scraper {
 
+    String USER_AGENT_STRING = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0";
+    String REFFERAL_URL = "http://www.google.com";
+
+    void startScraper();
+    void setWebSiteFromTerm();
+    void retrieveWebPage();
+    void scrapeCurrentPageAndReturnAsListOfClass();
+    void advanceToNextPage();
+    void setTerm(Term term);
+    void print(String message);
+    int getNumberOfClasses();
+    boolean isValidWebSiteWithClasses();
+
 }
