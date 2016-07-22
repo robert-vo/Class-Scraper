57d67c81021f1d0d47370682e1cf74a2661bb726
robert-vo <robertvo79@gmail.com>
2016-07-22 14:32:49 -0500

    Refactored methods to utilize streams for elements

--- src/scraper/StringUtilities.java
+++ src/scraper/StringUtilities.java
@@ -1,34 +1,29 @@
 package scraper;
 
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-/**
- * Created by Robert on 7/20/16.
- */
-public class StringUtilities extends Object {
+public class StringUtilities {
     static String REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES = "[\\(\\)]";
     static String REGEX_TO_GET_EMAIL_FROM_HREF_TAG       = "[\\t\\n\\r]";
     static String REGEX_FOR_BOTH_HYPHENS                 = "[-–]";
     static String REGEX_FOR_SPACES                       = "[ ]";
 
     static String extractTextBetweenParentheses(Elements e) {
-        try {
-            return e.first().text().split(REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES)[1];
-        }
-        catch (Exception e1) {
-            return "0";
-        }
+        return e.stream()
+                .findFirst()
+                .map(Element::text)
+                .map(str -> str.split(REGEX_TO_GET_CHARS_BETWEEN_PARENTHESES))
+                .orElse(new String[]{"0", "0"})[1];
     }
 
     static String extractEmailFromHREFTag(Elements e) {
-        try {
-            return e.attr("href")
-                    .replaceAll(REGEX_TO_GET_EMAIL_FROM_HREF_TAG, "")
-                    .substring(7);
-        }
-        catch (Exception e1) {
-            return "Unknown Email";
-        }
+        return e.stream()
+                .map(e1 -> e1.attr("href"))
+                .map(e1 -> e1.replaceAll(REGEX_TO_GET_EMAIL_FROM_HREF_TAG, ""))
+                .map(e1 -> e1.substring(7))
+                .findFirst()
+                .orElse("Unknown Email");
     }
 
     static String extractTextBetweenParentheses(String str) {
@@ -48,6 +43,7 @@ public class StringUtilities extends Object {
         if(isNullOrEmpty(wholeString)) {
             return "";
         }
+
         String[] splitString = wholeString.split(splitQualifier);
         if(splitString.length != 2) {
             return half ? splitString[0].trim() : "";
