ee0f2a4abc1fded0a3f480431a13197867d2ac2a
robert-vo <robertvo79@gmail.com>
2016-08-30 18:59:02 -0500

    Added documentation.

--- src/main/java/com/scraper/main/util/IntegerUtil.java
+++ src/main/java/com/scraper/main/util/IntegerUtil.java
@@ -1,8 +1,21 @@
 package com.scraper.main.util;
 
 
+/**
+ * IntegerUtil contains methods that will assist with transforming
+ * a String representation of an integer to an int.
+ *
+ * @author Robert Vo
+ */
 public class IntegerUtil {
 
+    /**
+     * Transforms a String representation of a number into an int.
+     * If the transformation fails due to an invalid String, the method will return 0.
+     *
+     * @param str The String representation of a number.
+     * @return An integer transformed from the String.
+     */
     public static int parseInt(String str) {
         try {
             return Integer.parseInt(str);
