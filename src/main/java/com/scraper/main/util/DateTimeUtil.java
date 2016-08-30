8549b643c057288e0cef785f44261d440a2b6b81
robert-vo <robertvo79@gmail.com>
2016-08-30 14:04:50 -0500

    Added documentation and logger

--- src/main/java/com/scraper/main/util/DateTimeUtil.java
+++ src/main/java/com/scraper/main/util/DateTimeUtil.java
@@ -1,14 +1,29 @@
 package com.scraper.main.util;
 
+import org.apache.log4j.Logger;
+
 import java.sql.Date;
 import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+/**
+ * DataTimeUtil contains methods that will assist with transforming
+ * a String representation of date/time to a valid java.sql.Date/java.sql.Time object.
+ *
+ * @author Robert Vo
+ */
 public class DateTimeUtil {
     final static String MONTH_DAY_YEAR_FORMAT = "MMMM d, yyyy";
     final static String TIME_FORMAT_12_HOUR   = "hh:mm a";
+    private static Logger log = Logger.getLogger(DateTimeUtil.class);
 
+    /**
+     * Transforms a String representation of a date to a valid java.sql.Date object.
+     *
+     * @param stringDate A String representation of a date.
+     * @return A java.sql.Date object that represents the String in the proper format.
+     */
     public static Date transformStringDateIntoSQLFormat(String stringDate) {
         final SimpleDateFormat format = new SimpleDateFormat(MONTH_DAY_YEAR_FORMAT, Locale.ENGLISH);
         try {
@@ -16,10 +31,18 @@ public class DateTimeUtil {
             return new java.sql.Date(date.getTime());
         }
         catch (Exception e) {
+            log.warn("Unable to convert date: " + stringDate);
+            log.warn("Given an error of: " + e);
             return null;
         }
     }
 
+    /**
+     * Transforms a String representation of a time to a valid java.sql.Time object.
+     *
+     * @param stringTime A String representation of a time.
+     * @return A java.sql.Time object that represents the String in the proper format.
+     */
     public static Time transformStringTimeIntoSQLFormat(String stringTime) {
         final SimpleDateFormat date12Format = new SimpleDateFormat(TIME_FORMAT_12_HOUR, Locale.ENGLISH);
         try {
@@ -28,6 +51,8 @@ public class DateTimeUtil {
             return new java.sql.Time(time.getTime());
         }
         catch (Exception e) {
+            log.warn("Unable to convert date: " + stringTime);
+            log.warn("Given an error of: " + e);
             return null;
         }
     }
