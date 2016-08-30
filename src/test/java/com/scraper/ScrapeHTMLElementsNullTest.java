3127ce7e2cf122867f0ed5fa4eae1cdad540a3b4
robert-vo <robertvo79@gmail.com>
2016-08-30 18:52:04 -0500

    Static imported parseInt

--- src/test/java/com/scraper/ScrapeHTMLElementsNullTest.java
+++ src/test/java/com/scraper/ScrapeHTMLElementsNullTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static com.scraper.main.ScrapeHTMLElements.*;
+import static com.scraper.main.util.IntegerUtil.parseInt;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
