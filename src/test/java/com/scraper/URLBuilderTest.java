cb1fe91c822bd27612a2850ca925f70a365a9e16
robert-vo <robertvo79@gmail.com>
2016-09-03 22:31:41 -0500

    Added tests for adding a session to terms and adding a subject to a URL.

--- src/test/java/com/scraper/URLBuilderTest.java
+++ src/test/java/com/scraper/URLBuilderTest.java
@@ -1,9 +1,6 @@
 package com.scraper;
 
-import com.scraper.main.Subject;
-import com.scraper.main.Term;
-import com.scraper.main.URLBuilder;
-import com.scraper.main.URLParameters;
+import com.scraper.main.*;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,10 +8,14 @@ import static org.junit.Assert.assertEquals;
 public class URLBuilderTest {
 
     final String URL_WITH_TERM_1990                 = "http://classbrowser.uh.edu/classes?term=1990";
+    final String URL_WITH_TERM_1990_SESSION_2       = "http://classbrowser.uh.edu/classes?term=1990-2";
     final String URL_WITH_TERM_2010                 = "http://classbrowser.uh.edu/classes?term=2010";
     final String URL_WITH_TERM_1990_SUBJECT_COSC    = "http://classbrowser.uh.edu/classes?term=1990&subject=COSC";
+    final String URL_WITH_TERM_1990_SUBJECT_COSC_SESSION_2    = "http://classbrowser.uh.edu/classes?term=1990-2&subject=COSC";
     final String URL_WITH_TERM_1990_PAGE_2          = "http://classbrowser.uh.edu/classes?term=1990&page=2";
     final String URL_WITH_TERM_2010_SUBJECT_COSC    = "http://classbrowser.uh.edu/classes?term=2010&subject=COSC";
+    final String URL_WITH_TERM_2010_SUBJECT_COSC_MIN_SESSION   = "http://classbrowser.uh.edu/classes?term=2010-MIN&subject=COSC&page=2";
+    final String URL_WITH_TERM_2010_SUBJECT_COSC_PAGE_2 = "http://classbrowser.uh.edu/classes?term=2010&subject=COSC&page=2";
     final String URL_WITH_TERM_2000_PAGE_2          = "http://classbrowser.uh.edu/classes?term=2000&page=2";
     final String URL_WITH_TERM_2000_PAGE_3          = "http://classbrowser.uh.edu/classes?term=2000&page=3";
 
@@ -30,12 +31,12 @@ public class URLBuilderTest {
 
     @Test
     public void testForTermEqualToSummer2016AndSubjectEqualToCOSC() {
-        assertEquals(URLBuilder.createURLForTermAndSubject(Term.SUMMER_2016, Subject.COSC.name()), URL_WITH_TERM_1990_SUBJECT_COSC);
+        assertEquals(URLBuilder.createURLForTermAndSubject(Term.SUMMER_2016, Subject.COSC), URL_WITH_TERM_1990_SUBJECT_COSC);
     }
 
     @Test
     public void testForTermEqualToSpring2017AndSubjectEqualToCOSC() {
-        assertEquals(URLBuilder.createURLForTermAndSubject(Term.SPRING_2017, Subject.COSC.name()), URL_WITH_TERM_2010_SUBJECT_COSC);
+        assertEquals(URLBuilder.createURLForTermAndSubject(Term.SPRING_2017, Subject.COSC), URL_WITH_TERM_2010_SUBJECT_COSC);
     }
 
     @Test
@@ -102,4 +103,47 @@ public class URLBuilderTest {
         assertEquals(URLBuilder.changePageNumber(URL_WITH_TERM_2010_SUBJECT_COSC, 123123),
                 URL_WITH_TERM_2010_SUBJECT_COSC + "&page=123123");
     }
+
+    @Test
+    public void testGenerateURLForSession2Term1990() {
+        String URL = URLBuilder.createURLForTermOnly(Term.SUMMER_2016);
+        URL = URLBuilder.modifyTermParameterValueForSession(URL, Session.SESSION_2);
+
+        assertEquals(URL_WITH_TERM_1990_SESSION_2, URL);
+    }
+
+    @Test
+    public void testGenerateURLForSession2SubjectCOSCTerm1990() {
+        String URL = URLBuilder.createURLForTermAndSubject(Term.SUMMER_2016, Subject.COSC);
+        URL = URLBuilder.modifyTermParameterValueForSession(URL, Session.SESSION_2);
+
+        assertEquals(URL_WITH_TERM_1990_SUBJECT_COSC_SESSION_2, URL);
+    }
+
+    @Test
+    public void testGenerateURLForSessionMinSubjectCOSCTerm2010Page2() {
+        String URL = URLBuilder.createURLForTermAndSubject(Term.SPRING_2017, Subject.COSC);
+        URL = URLBuilder.incrementPageNumber(URL);
+        URL = URLBuilder.modifyTermParameterValueForSession(URL, Session.MINI_SESSION);
+
+        assertEquals(URL_WITH_TERM_2010_SUBJECT_COSC_MIN_SESSION, URL);
+    }
+
+    @Test
+    public void testAddSubjectParameterToURLGivenCOSCTerm2010() {
+        String URL = URLBuilder.createURLForTermOnly(Term.SPRING_2017);
+        URL = URLBuilder.addSubjectParameterToURL(URL, Subject.COSC);
+
+        assertEquals(URL_WITH_TERM_2010_SUBJECT_COSC, URL);
+    }
+
+    @Test
+    public void testAddSubjectParameterToURLGivenCOSCTerm2010Page2() {
+        String URL = URLBuilder.createURLForTermOnly(Term.SPRING_2017);
+        URL = URLBuilder.addSubjectParameterToURL(URL, Subject.COSC);
+        URL = URLBuilder.changePageNumber(URL, 2);
+
+        assertEquals(URL_WITH_TERM_2010_SUBJECT_COSC_PAGE_2, URL);
+    }
+
 }
